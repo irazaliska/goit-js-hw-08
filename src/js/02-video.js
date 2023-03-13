772afaac18e35271d6c7975f5e165beb004b997a
Iryna Zaliska <zaliska@gmail.com>
2023-03-14 00:37:07 +0200

    hw-08-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+
+function getCurrentTime(event) {
+  localStorage.setItem(CURRENT_TIME, event.seconds);
+}
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
